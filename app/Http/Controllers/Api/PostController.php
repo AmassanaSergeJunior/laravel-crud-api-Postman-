67100e5b0f7ac5d04def54461ade587d8f8cbc10
Amassana Serge Junior <amassanasergejunior@gmail.com>
2024-06-13 18:19:19 +0100

    adding code to this pages

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -13,11 +13,13 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return PostResource::collection($posts);
     }
 
     /**
      * Show the form for creating a new resource.
+      * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -26,10 +28,14 @@ class PostController extends Controller
 
     /**
      * Store a newly created resource in storage.
+       * @param  \Illuminate\Http\Request  $request
+       * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $posts = Post::create($request->all());
+        
+        return new PostResource($posts);
     }
 
     /**
@@ -42,6 +48,8 @@ class PostController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+        * @param  \App\Models\Post  $post
+        * @return \Illuminate\Http\Response
      */
     public function edit(Post $post)
     {
@@ -50,17 +58,26 @@ class PostController extends Controller
 
     /**
      * Update the specified resource in storage.
+      * @param  \Illuminate\Http\Request  $request
+      * @param  \App\Models\Post  $post
+      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $post->update($request->all());
+        
+        return new PostResource($post);
     }
 
     /**
      * Remove the specified resource from storage.
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response(null, 204);
     }
 }
